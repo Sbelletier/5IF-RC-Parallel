218ab78008c59125202d590a46d58047d3a217fc
Belletier Simon <sbelletier@if501-219-07.insa-lyon.fr>
2017-12-05 10:49:17 +0100

    modified cmake to include mpi (no mpi code yet)

---
+++ exportMPI.sh
@@ -0,0 +1,7 @@
+#installed using http://lsi.ugr.es/jmantas/pdp/ayuda/datos/instalaciones/Install_OpenMPI_en.pdf
+# Use -LLIBDIR
+echo "Call to exportMPI.sh"
+export PATH="$PATH:/home/$USER/.ompi/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/$USER/.ompi/lib64/"
+export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:/home/sbelletier/.ompi/"
+
